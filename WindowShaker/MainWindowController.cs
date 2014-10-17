b9ecee44cd3ffcb85c85efc5787f3daa66c49788
Mike James <mike@xamarin.com>
2014-10-17 16:49:18 +0100

    Created Xamarin.Mac Library for shake effect
    the Shake effect is now an extensions method for any NSWindows.

--- WindowShaker/MainWindowController.cs
+++ WindowShaker/MainWindowController.cs
@@ -56,31 +56,11 @@ namespace WindowShaker
 		{
 			if(tbxPassword.StringValue.Length > 0)
 			{
-				Window.Animations = NSDictionary.FromObjectAndKey(ShakeAnimation(this.Window.Frame), new NSString(@"frameOrigin"));
-				((NSWindow)this.Window.Animator).SetFrameOrigin(Window.Frame.Location);
+                this.Window.Shake();
 			}
 		}
 
-		CAKeyFrameAnimation ShakeAnimation(RectangleF frame)
-		{
-			var numberOfShakes = 3;
-			var durationOfShakes = 0.5f;
-			var vigourOfShake = 0.02f;
-			CAKeyFrameAnimation shakeAnimation = new CAKeyFrameAnimation();
-
-			var shakePath = new CGPath();
-			shakePath.MoveToPoint(frame.GetMinX(), frame.GetMinY());
-			int index;
-			for (index = 0; index < numberOfShakes; index++)
-			{
-				shakePath.CGPathAddLineToPoint(frame.GetMinX() - frame.Size.Width * vigourOfShake, frame.GetMinY());
-				shakePath.CGPathAddLineToPoint(frame.GetMinX() + frame.Size.Width * vigourOfShake, frame.GetMinY());
-			}
-			shakePath.CloseSubpath();
-			shakeAnimation.Path = shakePath;
-			shakeAnimation.Duration = durationOfShakes;
-			return shakeAnimation;
-		}
+		
     }
 }
 
